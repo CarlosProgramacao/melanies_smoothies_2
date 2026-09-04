a3ec06f7c895d6b0ad402e4c869d7df2bfd34ab9
CarlosProgramacao <141882239+CarlosProgramacao@users.noreply.github.com>
2026-09-04 18:27:53 +0100

    Update streamlit_app.py
    add api

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,7 @@ if options:
     ).collect()
         
         st.success(f'Order submitted, {name_on_order}!', icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
