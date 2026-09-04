74e189097fd75a2600499c5fb25a60849dfa53e1
CarlosProgramacao <141882239+CarlosProgramacao@users.noreply.github.com>
2026-09-04 18:29:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ if options:
         
         st.success(f'Order submitted, {name_on_order}!', icon="✅")
 
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response.json())
